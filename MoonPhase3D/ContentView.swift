f2414ee11ae0442f08fd989a59f1e344de1a934f
James Bush <jamesalanbush1972@gmail.com>
2025-10-30 17:05:13 -0400

    Initial Commit

---
+++ MoonPhase3D/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MoonPhase3D
+//
+//  Created by Xcode Developer on 10/30/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MoonPhase3D/MoonPhase3DApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonPhase3DApp.swift
+//  MoonPhase3D
+//
+//  Created by Xcode Developer on 10/30/25.
+//
+
+import SwiftUI
+
+@main
+struct MoonPhase3DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
